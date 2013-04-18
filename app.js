b27de823f76815d2e8240cf505214ce9386c3f23
Jeff Harris <techjeffharris@gmail.com>
2013-04-18 09:47:06 -0700

    Merge branch 'master' of https://github.com/adminion/adminion

--- app.js
+++ app.js
@@ -4,49 +4,32 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
 
-var app = module.exports = express.createServer();
-var io = require('socket.io').listen(app);
+var app = express();
 
-// Configuration
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
+// development only
+if ('development' == app.get('env')) {
   app.use(express.errorHandler());
-});
-
-// Routes
+}
 
 app.get('/', routes.index);
+app.get('/users', user.list);
 
-// socket.io
-
-io.sockets.on('connection', function (socket) {
-    console.log("socket: %j", socket);
-    io.sockets.emit('news', { will : 'be received by everyone'}
-  );
-
-  socket.on('private message', function (from, msg) {
-    console.log('I received a private message by ', from, ' saying ', msg);
-  });
-
-  socket.on('disconnect', function () {
-    io.sockets.emit('user disconnected');
-  });
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-app.listen(1337);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
