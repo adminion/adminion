2de9bba040c41f724dff8f1b92587db3c20746aa
Jeff Harris <techjeffharris@gmail.com>
2013-04-22 17:00:32 -0700

    started building the lobby structure, pushing c9...

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Adminion' });
 };
+
+exports.lobby = function(req, res){
+  res.render('lobby', {title: 'Game Lobby | Adminion' });
+};
