461793c8df38191821d05e0b194759bea3881da1
Jeff Harris <jeff@intangiblehost.net>
2013-06-04 21:25:38 -0700

    adminion is an event emitter
    decided to stop tracking node_modules
    lots of other stuff

--- app.js
+++ app.js
@@ -1,53 +1,12 @@
 
-// load core node modules
-var http = require('http')
-	, https = require('https');
+//console.log('app.js - global');
+//console.log(global);
 
 // load main library and controllers
-var adminion = require('./lib/')
-	, game = require('./controllers/');
+var Adminion = require('./lib/');
 
-// breaking main library into peices for easier, simpler, and smoother code
-var app = adminion.app
-	, auth = adminion.auth
-	, config = adminion.config
-	, env = adminion.env;
 
-// make the properties this object available as variables in all views
-app.locals = {env: env};
-
-// define all application routes
-app.get('/', game.get.root);
-app.get('/logon', game.get.logon);
-
-// note that this is a POST request...
-app.post('/logon', auth.passport.authenticate('local'), game.post.logon);
-
-app.get('/logoff', game.get.logoff);
-
-/**
- * authorization required
- */
-app.get('/join', auth.verify, game.get.joinGame);
-app.get('/lobby', auth.verify, game.get.lobby);
-app.get('/play', auth.verify, game.get.play);
-app.get('/spectate', auth.verify, game.get.spectate);
-
-// display configuration settings for debugging purposes...
-//console.log('app.js 62 - config', config);
-
-/**
- * this is where Cluster code will go, eventually...
- */
-
-// if https was enabled in the config
-if (config.https) {
-	// create https server instance, then listen!
-	https.createServer(config.https, app).listen(config.port, adminion.listening);
-} else {
-	// create http server instance, then listen!
-	http.createServer(app).listen(config.port, adminion.listening);
-}
+Adminion.startGame();
 
 // now sit back and wait for requests!
 
--- models/player.js
+++ models/player.js
@@ -1,31 +1,35 @@
-/**
- * define the object structure of the Player Schema
+/** 
+ * 	models/player.js - define the adminion Player Schema
  * 
+ * @see https://github.com/saintedlama/passport-local-mongoose
  */
 
-var mongoose = require('mongoose')
-	, Schema = mongoose.Schema
-	, passportLocalMongoose = require('passport-local-mongoose')
+var passportLocalMongoose = require('passport-local-mongoose');
 
-var PlayerSchema = new Schema({
-	firstName: String,
-	lastName: String,
-	email: {type: String, unique : true},
-	password: String,
-	handle: {type: String, unique : true},
-	stats: {
-		created : Date,
-		gamesPlayed: Number,
-		gamesWon: Number,
-		mostPoints: Number,
-		totalPoints: Number,
-		totalPlayTime: Number
-	}
-});
+// export the Player constructor
+module.exports = function(mongoose) {
+	var self = this;
+	
+	// define the PlayerSchema
+	var PlayerSchema = new mongoose.Schema({
+		firstName: String,
+		lastName: String,
+		handle: {type: String, unique : true},
+		stats: {
+			created : Date,
+			gamesPlayed: Number,
+			gamesWon: Number,
+			mostPoints: Number,
+			totalPoints: Number,
+			totalPlayTime: Number
+		}
+	});
+	
+	// now plugin the passportLocalMongoose functionality
+	PlayerSchema.plugin(passportLocalMongoose
+		, { usernameField : 'email' }
+	);
 
-// now plugin the passportLocalMongoose functionality
-PlayerSchema.plugin(passportLocalMongoose);
-
-// and finally create a model from our PlayerSchema
-module.exports = mongoose.model('Player', PlayerSchema);
-
+	// and finally return a model created from PlayerSchema
+	return mongoose.model('Player', PlayerSchema);
+};
