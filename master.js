dff6c4a78d6a08b3f312c1edab65211292ce3ac4
Jeff Harris <jeff@intangiblehost.net>
2013-11-18 19:36:21 -0800

    really just committing to check out a working copy

--- master.js
+++ master.js
@@ -9,17 +9,17 @@ cluster.setupMaster({
 	silent: false
 });
 
-// fork the number of workers as defined in config module
-for (var i = 0; i < config.workers; i += 1) {
-    cluster.fork();
-}
-
 cluster.on('fork', function (worker) {
 	debug.emit('marker', 'Forking worker ' + worker.id + '.', 'app', 26);
 });
 
 cluster.on('online', function (worker) {
 	debug.emit('marker', 'Worker ' + worker.id + ' started.', 'app.js', 30);
+
+	// once online, start the next worker
+	if (cluster.workers.length < config.workers) {
+		cluster.fork();
+	}
 });
 
 cluster.on('exit', function (worker) {
--- worker.js
+++ worker.js
@@ -7,5 +7,7 @@ var adminionServer = require('./lib/');
 // create a server instance
 var Adminion = adminionServer();
 
+debug.emit('val', 'Adminion', Adminion, 'worker.js', 10);
+
 // now sit back and wait for requests
 Adminion.start();
