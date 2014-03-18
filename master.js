858350dad8a857de9288dd4b35892b3653d74397
Jeff Harris <techjeffharris@gmail.com>
2014-03-18 11:01:07 -0700

    its been a while, and i'm going to do laundry

--- master.js
+++ master.js
@@ -3,184 +3,45 @@ console.log('Starting Adminion game server...');
 
 global.debug = require('./lib/debug')();
 
-var cluster = require('cluster'),
-    util = require('util');
+var util = require('util')
 
-var config = require('./lib/config'),
+var ClusterFuck = require('cluster-fuck'),
+    config = require('./lib/config'),
     env = require('./lib/env'),
     utils = require('./lib/utils');
 
-var memory = {};
+var adminionCluster = new ClusterFuck(config.cluster);
 
-var deadWorkers;
-var readyWorkers;
-var suicide;
-
-cluster.setupMaster({
-    exec: "worker.js",
-    silent: (config.debug) ? false : true
+adminionCluster.on('ready', function onClusterReady () {
+    console.log('Adminion Cluster is now ready!');
 });
 
-cluster.on('fork', function fork (worker) {
-    // debug.emit('msg', 'Forking worker ' + worker.id + '.');
+adminionCluster.on('starting', function onClusterStarting () {
+    console.log('starting cluster...');
 });
 
-cluster.on('online', function online (worker) {
-    // debug.emit('msg', 'Worker ' + worker.id + ' online.');
-    initWorker(worker);
+adminionCluster.on('restarting', function onClusterRestarting () {
+    console.log('restarting cluster...');
 });
 
-cluster.on('disconnect', function disconnect (worker) {
-    debug.emit('msg', 'Worker ' + worker.id + ' disconnected.');
-    delete memory[worker.id];
+adminionCluster.on('restarted', function onClusterRestarted () {
+    console.log('cluster restarted!');
 });
 
-cluster.on('exit', function exit (worker) {
-    debug.emit('msg', 'Worker ' + worker.id + ' died.');
-    
-    delete memory[worker.id];
-    
-    deadWorkers +=1;
-
-    if (suicide) {
-        if (deadWorkers === config.workers) {
-            process.kill();
-        }
-    } else {
-        cluster.fork();
-    }
-
+adminionCluster.on('stopping', function onClusterStopping () {
+    console.log('stopping cluster...');
 });
 
-function restart (worker) { 
-    var id;
+adminionCluster.on('stopped', function onClusterStopped () {
+    console.log('cluster stopped!');
+});
 
-    console.log('restarting the server...');
+adminionCluster.on('killing', function onClusterKilling () {
+    console.log('killing cluster...');
+});
 
-    // if a given worker was specified
-    if (worker) {
-        worker.kill();
-        cluster.fork();
-    } else {
-        for (id in cluster.workers) {
-            cluster.workers[id].kill();
-        }
+adminionCluster.on('killed', function onClusterKilled () {
+    console.log('cluster killed!');
+});
 
-        startWorkers();
-    }
-};
-
-function stop () {
-    var id;
-
-    suicide = true;
-    deadWorkers = 0;
-
-    for (id in cluster.workers) {
-        cluster.workers[id].kill();
-    }
-
-};
-
-function totalMemory () {
-
-    var masterMemory = process.memoryUsage().heapTotal,
-        serverTotal,
-        workerId,
-        workerTotal = 0;
-
-    // debug.emit('val', 'masterMemory', masterMemory, 'master.js', 73);
-    // debug.emit('val', 'memory', memory, 'master.js', 74);
-    // debug.emit('val', 'workerTotal', workerTotal, 'master.js', 75);
-
-    for (workerId in memory) {
-        workerTotal += memory[workerId];
-
-        // debug.emit('val', 'workerId', workerId, 'master', 78);
-        // debug.emit('val', 'memory[' + workerId + ']', memory[workerId], 'master', 79);
-        // debug.emit('val', 'workerTotal', workerTotal, 'master', 81);
-
-    }
-
-    serverTotal = masterMemory + workerTotal;
-
-    // debug.emit('val', 'serverTotal', serverTotal, 'master', 89);
-
-    return serverTotal / utils.MB;
-};
-
-function allReady () {
-    var msg = '\n\t%s worker';
-
-    if ( config.workers > 1 ) {
-        msg += 's';
-    }
-
-    msg += ' running.';
-    
-    console.log(util.format(msg, config.workers));
-
-    console.log('\nAdminion Game Server Started!\n --> %s', env.url());
-
-    console.log('\ntotal memory usage: %s MB', totalMemory());
-
-    process.stdout.write('> ');
-
-    process.stdin.resume();
-    process.stdin.setEncoding('utf8');
-
-    process.stdin.on('data', function stdinData(chunk) {
-        switch (chunk.trim()) { 
-            case 'restart':
-                restart();
-            break;
-
-            case 'stop':
-                stop();
-            break;
-        }
-
-        process.stdout.write('> ');
-    });
-
-    process.stdin.on('end', function stdinEnd() {
-        process.stdout.write('end');
-    });
-};
-
-
-
-function initWorker (worker) {
-    // when a message from this worker is received
-    worker.on('message', function message (data) {
-
-        // debug.emit('msg', 'message received from worker ' + workerId, 'master.js', 132);
-        
-        // if the worker is saying it is ready...
-        if (data['ready'] && data['ready'] === true) {
-            // add 1 to the readyWorkers count
-            readyWorkers += 1;
-            
-            memory[worker.id] = data['memoryUsage'];
-
-            // if all of our workers are ready...
-            if (readyWorkers === config.workers) {
-                allReady();
-            }       
-        }
-    });
-};
-
-function startWorkers() {
-
-    var forked = 0;
-    readyWorkers = 0;
-
-    // start all the workers
-    while (forked < config.workers) {
-        cluster.fork();
-        forked +=1;
-    }
-}
-
-startWorkers();
+adminionCluster.start();
--- worker.js
+++ worker.js
@@ -1,4 +1,5 @@
 
+console.log('needle');
 var cluster = require('cluster');
 
 if (cluster.isMaster) {
@@ -13,14 +14,18 @@ global.debug = require('./lib/debug')();
 
 debug.emit('val', 'AdminionServer', AdminionServer);
 
-AdminionServer.on('error', AdminionServer.kill);
+var server = AdminionServer();
 
-AdminionServer.on('ready', function ready () {
-        var mem = process.memoryUsage();
+debug.emit('val', 'server', server);
+
+server.on('error', server.kill);
+
+server.on('ready', function ready () {
+    var mem = process.memoryUsage();
 
     if (cluster.isMaster) {
 
-        console.log('Adminion Game Server ready --> ', AdminionServer.env.url());
+        console.log('Adminion Game Server ready --> ', server.env.url());
 
         debug.emit('msg', util.format('total memory usage: %d MB', mem.heapTotal / utils.MB));
 
@@ -34,4 +39,4 @@ AdminionServer.on('ready', function ready () {
     return true;
 });
 
-AdminionServer.start();
+server.start();
