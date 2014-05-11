5a737e88b286b4ba921ceeea0d38d7553690a078
Jeff Harris <techjeffharris@gmail.com>
2014-05-10 19:38:38 -0700

    new branch v0.7.x

--- server.js
+++ server.js
@@ -1,135 +1,52 @@
 
-global.debug = require('./lib/debug')();
-
-var cluster = require('cluster'),
+var Adminion = require('./lib/'),
+    config = require('./lib/config'),
+    env = require('./lib/env'),
+    prepl  = require('prepl'),
+    util = require('util'),
+    utils = require('./lib/utils'),
     interrupt,
-    util = require('util');
+    server;
 
-var ClusterFuck = require('cluster-fuck'),
-    config = require('./lib/config').cluster;
+global.debug = require('./lib/debug')(config.debug);
 
 process.on('SIGINT', function () {
-    if (cluster.isMaster) {
-        if (interrupt) {
-            adminionCluster.shutdown(process.exit);
-        } else {
-            console.log('\n(^C again to quit)'); 
-            interrupt = setTimeout(function () {
-                interrupt = undefined;
-            }, 1000);
-        }
-        
+    if (interrupt) {
+        server.stop(process.exit);
     } else {
-        // it seems as though the master forwards SIGINT to all workers immediately, 
-        // so telling workers to do nothing on SIGINT is a way of testing this hypothesis,
-        // but not a good idea in production, definitely 
-        server.status();
+        console.log('\n(^C again to quit)'); 
+        interrupt = setTimeout(function () {
+            interrupt = undefined;
+        }, 1000);
     }
+        
 });
 
-if (cluster.isMaster) {
-    console.log('Starting Adminion game server...');
-
-    var adminionCluster = new ClusterFuck(config);
-
-    adminionCluster.on('ready', function onClusterReady () {
-        console.log('Adminion Cluster is now ready!');
-
-        var memory = adminionCluster.memoryUsage('MB');
-
-        debug.emit('msg', util.format('total memory usage: %d MB', memory ));
-    });
-
-    adminionCluster.on('starting', function onClusterStarting () {
-        console.log('starting cluster...');
-    });
-
-    adminionCluster.on('restarting', function onClusterRestarting () {
-        console.log('restarting workers...');
-    });
-
-    adminionCluster.on('restarted', function onClusterRestarted () {
-        console.log('workers restarted!');
-    });
-
-    adminionCluster.on('stopping', function onClusterStopping () {
-        console.log('stopping workers...');
-    });
-
-    adminionCluster.on('shuttingDown', function onClusterShuttingDown () {
-        console.log('shutting down cluster...');
-    });
+// debug.emit('val', 'config', config)
     
-    adminionCluster.on('shutdown', function onClusterShutdown () {
-        console.log('cluster shut down!');
-    });
+console.log('Starting Adminion game server...');
 
-    adminionCluster.on('stopped', function onClusterStopped () {
-        console.log('workers stopped!');
-    });
+debug.emit('val', 'Adminion', Adminion);
 
-    adminionCluster.on('killing', function onClusterKilling () {
-        console.log('killing workers...');
-    });
+server = new Adminion();
 
-    adminionCluster.on('killed', function onClusterKilled () {
-        console.log('workers killed!');
-    });
+server.setMaxListeners(0);
 
-    adminionCluster.on('fork', function onWorkerFork (worker) {
-        console.log('worker %s forked!', worker.id);
-    });
+// debug.emit('val', 'server', server);
 
-    adminionCluster.on('online', function onWorkerOnline (worker) {
-       console.log('worker %s online!', worker.id); 
-    });
+server.on('error', server.kill);
 
-    adminionCluster.on('disconnect', function onWorkerDisconnect (worker) {
-       console.log('worker %s disconnected!', worker.id); 
-    });    
+server.on('ready', function ready () {
 
-    adminionCluster.on('exit', function onWorkerExit (worker) {
-       console.log('worker %s exited!', worker.id); 
-    });
-
-    adminionCluster.start();
+    console.log('Adminion game server started: %s', env.url())
     
-} else {
+    debug.emit('msg', util.format('total memory usage: %d MB', process.memoryUsage().rss / utils.MB ));
+    
+    return true;
+});
 
-    var AdminionServer = require('./lib/'),
-        utils = require('./lib/utils');
+server.on('stopped', function stopped () {
+    process.exit();
+});
 
-    debug.emit('val', 'AdminionServer', AdminionServer);
-
-    var server = AdminionServer();
-
-    server.setMaxListeners(0);
-
-    debug.emit('val', 'server', server);
-
-    server.on('error', server.kill);
-
-    server.on('ready', function ready () {
-        
-        var pieces = [
-            'worker', 
-            cluster.worker.id, 
-            'ready -->', 
-            server.env().url()
-        ];
-
-        debug.emit('msg', pieces.join(' '));
-
-        process.send({'ready': true, 'memoryUsage': process.memoryUsage()});
-        
-        return true;
-    });
-
-    server.on('stopped', function stopped () {
-        process.exit();
-    });
-
-    server.start();
-
-}
-
+server.start();
